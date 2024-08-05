d0dd97c5e087d62efb23306cb85a774428fcf5bf
SuryanshSS1011 <159204949+SuryanshSS1011@users.noreply.github.com>
2024-08-05 17:29:37 -0400

    Updated file path for client.jsx

--- pages/ViewCreator.jsx
+++ pages/ViewCreator.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { supabase } from "../client";
+import { supabase } from "../components/client";
 import "./ViewCreator.css";
 
 const ViewCreator = () => {
